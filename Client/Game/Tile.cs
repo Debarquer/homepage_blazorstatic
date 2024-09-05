9cc48f4c6c809b2a10558c30220a79a96a5bcd6f
Simon Ektjärn <debarquer@gmail.com>
2024-09-06 01:13:01 +0200

    Added game and updated homepage

---
+++ Client/Game/Tile.cs
@@ -0,0 +1,50 @@
+﻿using static BlazorApp.Client.Pages.Game;
+
+namespace BlazorApp.Client.Game
+{
+    public class Tile(char gfx, string color)
+    {
+        public char GFX { get; private set; } = gfx;
+        public string Color { get; private set; } = color;
+
+        public int lifetime = 0;
+
+        virtual public void SetGfx(char gfx)
+        {
+            this.GFX = gfx;
+        }
+
+        virtual public void SetColor(string color)
+        {
+            this.Color = color;
+        }
+
+        virtual public void Update()
+        {
+            lifetime++;
+        }
+
+        virtual public void OnPlayerEnter()
+        {
+            lifetime = 0;
+        }
+    }
+
+    public class TreasureTile(char gfx, string color, PlayerInventory playerInventory, OutputConsole outputConsole, Map map, MapPosition playerPosition): Tile(gfx, color)
+    {
+        public PlayerInventory PlayerInventory { get; private set; } = playerInventory;
+        public OutputConsole OutputConsole { get; private set; } = outputConsole;
+        public Map Map { get; private set; }
+        public MapPosition PlayerPosition { get; private set; } = playerPosition;
+
+        override public void OnPlayerEnter()
+        {
+            base.OnPlayerEnter();
+
+            OutputConsole.output.Add("You have found a treasure chest containing 10 gold!");
+            PlayerInventory.gold += 10;
+            //Map.map[(PlayerPosition.row, PlayerPosition.column)].SetGfx('g');
+            //Map.map[(PlayerPosition.row, PlayerPosition.column)].SetColor("green");
+        }
+    }
+}
